2285548889fb051c3964e42db73c8be0af5088e0
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-02-27 05:18:04 -0500

    Update SporkServiceProvider.php

--- src/SporkServiceProvider.php
+++ src/SporkServiceProvider.php
@@ -15,11 +15,11 @@ class SporkServiceProvider extends RouteServiceProvider
     
     public function register()
     {
-        Spork::addFeature('__FEATURE_NAME__', 'ViewBoardsIcon', '/__FEATURE_NAME__');
+        Spork::addFeature('__FEATURE_NAME__', 'ViewBoardsIcon', '/__FEATURE_NAME_SLUG__');
       
         if (config('spork.__FEATURE_NAME__.enabled')) {
-            Route::middleware($this->app->make('config')->get('spork.__FEATURE_NAME__.middleware', ['auth:sanctum']))
-                ->prefix('api/__FEATURE_NAME__')
+            Route::middleware($this->app->make('config')->get('spork.__FEATURE_NAME_SLUG__.middleware', ['auth:sanctum']))
+                ->prefix('api/__FEATURE_NAME_SLUG__')
                 ->group(__DIR__ . '/../routes/web.php');
         }
     }
