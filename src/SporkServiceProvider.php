8a9dbec11b8da8a5cbf1d59c31bb46ea7f9530ac
austinkregel <austinkregel@users.noreply.github.com>
2022-09-17 06:08:44 +0000

    cs fixes pls

--- src/SporkServiceProvider.php
+++ src/SporkServiceProvider.php
@@ -12,15 +12,15 @@ class SporkServiceProvider extends RouteServiceProvider
     {
       //
     }
-    
+
     public function register()
     {
         Spork::addFeature('__FEATURE_NAME__', 'ViewBoardsIcon', '/__FEATURE_NAME_SLUG__');
-      
+
         if (config('spork.__FEATURE_NAME__.enabled')) {
             Route::middleware($this->app->make('config')->get('spork.__FEATURE_NAME_SLUG__.middleware', ['auth:sanctum']))
                 ->prefix('api/__FEATURE_NAME_SLUG__')
-                ->group(__DIR__ . '/../routes/web.php');
+                ->group(__DIR__.'/../routes/web.php');
         }
     }
 }
